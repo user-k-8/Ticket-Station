53c2e2bd582fe07805cc0f1a89e5b4c233fefc3c
Khanyisile <121167505+user-k-8@users.noreply.github.com>
2023-08-18 17:41:27 +0200

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -5,14 +5,15 @@ let navCollapsed = "false";
 
 navExpandButton.addEventListener('click',  ()=> {
   if (navCollapsed=="true") {
-   navItems.style.display="none"
+   navItems.style.display="none";
     
-    navExpandButton.ariaExpanded= "false"
+    navExpandButton.ariaExpanded= "false";
      navCollapsed = "false";
   } else {  
     navCollapsed = "true";
-    navItems.style.display="block"
-    navItems.style.width ="100%"
+    navItems.style.display="block";
+    navItems.style.width ="100%";
+   navItems.style.backgroundColor="#6A040F"
     navExpandButton.ariaExpanded= "true"
     
   }
